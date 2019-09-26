61b67b89da32a0dc3503c4dbfc05a31c63c53c72
navaneeth <navaneeth.info>
2019-09-26 20:18:17 +0530

    added plugins for pom.xml file

---
+++ src/test/java/Automation/pipeline/Sample.java
@@ -0,0 +1,12 @@
+package Automation.pipeline;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test
+	public void testEx()
+	{
+		System.out.println("running through jenkins");
+	}
+
+}
